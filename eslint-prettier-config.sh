4c5da141b5727dadf65abfdac0a896521be1ff67
ghocevar <hocevar.gregi@gmail.com>
2020-06-25 15:26:37 +0200

    Added space at the end of a question

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -53,7 +53,7 @@ finished=false
 
 # Max Line Length Prompt
 while ! $finished; do
-  read -p "What max line length do you want to set for ESLint and Prettier? (Recommendation: 80)"
+  read -p "What max line length do you want to set for ESLint and Prettier? (Recommendation: 80) "
   if [[ $REPLY =~ ^[0-9]{2,3}$ ]]; then
     max_len_val=$REPLY
     finished=true
